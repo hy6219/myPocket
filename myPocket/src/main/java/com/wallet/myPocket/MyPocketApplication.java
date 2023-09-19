7732cf6e75e437c3d98e0cc8f2db55e30b2d4b30
hy6219 <hy6219@naver.com>
2023-09-20 01:54:50 +0900

    chore: 프로젝트 생성

---
+++ myPocket/src/main/java/com/wallet/myPocket/MyPocketApplication.java
@@ -0,0 +1,13 @@
+package com.wallet.myPocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPocketApplication.class, args);
+	}
+
+}
---
+++ myPocket/src/test/java/com/wallet/myPocket/MyPocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wallet.myPocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
